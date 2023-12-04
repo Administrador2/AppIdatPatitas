b9c07f3e68e735f138ef3edb29a96b63c853142b
Administrador2 <daplicacionesmoviles1@gmail.com>
2023-12-04 18:04:24 -0500

    metodos ROOM

---
+++ app/src/main/java/pe/edu/idat/appidatpatitas/bd/entity/Persona.java
@@ -0,0 +1,18 @@
+package pe.edu.idat.appidatpatitas.bd.entity;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "persona")
+public class Persona {
+    @PrimaryKey
+    private int id;
+    private String nombres;
+    private String apellidos;
+    private String email;
+    private String celular;
+    private String usuario;
+    private String password;
+    private String esvoluntario;
+
+}
